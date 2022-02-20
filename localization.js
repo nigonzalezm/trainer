45ec77db38e97aad92a632380c486886d4761167
Nelson González <nelson.gonzalez@nextialab.com>
2022-02-20 11:27:31 -0600

    Adding recover option to trainer, adding iterations and reset to localization run

--- localization.js
+++ localization.js
@@ -9,14 +9,17 @@ function randomPosition() {
 
 async function script() {
     await trainer.init();
-    await trainer.waitFor(10);
-    await trainer.changeTo('play_on');
-    for (let i = 0; i < 10; i++) {
-        let position = randomPosition();
-        await trainer.moveBallTo(position.x, position.y);
-        await trainer.waitFor(50);
+    for (let iteration = 0; iteration < 100; iteration++) {
+        await trainer.waitFor(10);
+        await trainer.changeTo('play_on');
+        for (let move = 0; move < 60; move++) {
+            let position = randomPosition();
+            await trainer.moveBallTo(position.x, position.y);
+            await trainer.waitFor(50);
+        }
+        await trainer.changeTo('before_kick_off');
+        await trainer.recover();
     }
-    await trainer.changeTo('before_kick_off');
     await trainer.say('finish');
     setTimeout(() => { trainer.close(); }, 1000);
 }
