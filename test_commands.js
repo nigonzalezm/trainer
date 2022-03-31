c31669e3043fa205b9c860f4e0e0c7992aa02c3c
Nelson González <nelson.gonzalez@nextialab.com>
2022-03-30 18:48:45 -0600

    Removing waits between commands in test_commands

--- test_commands.js
+++ test_commands.js
@@ -5,13 +5,10 @@ async function script() {
     await trainer.waitFor(10);
     await trainer.commandMoveTo(20, 20);
     await trainer.changeTo('play_on');
-    await trainer.waitFor(100);
     await trainer.commandKickBallTo(-30, -30);
-    await trainer.waitFor(300);
     await trainer.commandMoveTo(20, -20);
-    await trainer.waitFor(100);
     await trainer.commandKickBallTo(30, 30);
-    await trainer.waitFor(300);
+    await trainer.waitFor(1000);
     await trainer.changeTo('before_kick_off');
     await trainer.say('sm finish');
     await trainer.close();
